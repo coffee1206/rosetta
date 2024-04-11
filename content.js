6ed55cd031091f2fce397df8d75c0b98da91af88
coffee1206 <cafemist1206@gmail.com>
2024-04-11 23:56:05 +0900

    タグがついていないテキストにも対応

--- content.js
+++ content.js
@@ -18,41 +18,49 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // 各要素を取得、翻訳して置換する
 function exec() {
   console.time("exec_time");
-  const selectors = ["h1", "h2", "h3", "p", "li"];
-  const rawArray = [];
+  const allNodes = Array.from(
+    document.querySelectorAll(
+      "body *:not(script):not(script *):not(code):not(code *):not(style):not(style *):not(link):not(meta):not(noscript):not(iframe):not(object):not(embed)"
+    )
+  )
+    .map((el) => Array.from(el.childNodes).filter((el) => el instanceof Text))
+    .flat();
+  const textNodes = [];
+  const rawTextArray = [];
   const promises = [];
 
-  selectors.forEach((selector) => {
-    rawArray.length = 0;
-    document.querySelectorAll(selector).forEach((element) => {
-      rawArray.push(element.textContent);
-    });
-    const promise = translate(rawArray)
-      .then((translation) => {
-        const translatedArray = JSON.parse(translation.translatedText);
-        console.log("selector:" + selector);
-        console.log("input_tokens:" + translation.input_tokens);
-        console.log("output_tokens:" + translation.output_tokens);
-        console.log(translatedArray);
-        replaceContent(selector, translatedArray);
-        console.timeLog("exec_time");
-      })
-      .catch((error) => {
-        element.textContent = error;
-      });
-    promises.push(promise);
+  allNodes.forEach((element) => {
+    const textNodeContent = element.textContent.trim();
+    if (textNodeContent !== "") {
+      textNodes.push(element);
+      rawTextArray.push(textNodeContent);
+    }
   });
+
+  // console.log(rawTextArray);
+  const promise = translate(rawTextArray)
+    .then((response) => {
+      //TODO テキストノードベースに変更したがここでコケてる　あとで直す
+      const translatedArray = JSON.parse(response.translatedText);
+      console.log("input_tokens:" + response.input_tokens);
+      console.log("output_tokens:" + response.output_tokens);
+      console.log(translatedArray);
+      replaceContent(textNodes, translatedArray);
+      console.timeLog("exec_time");
+    })
+    .catch((error) => {
+      element.textContent = error;
+    });
+  promises.push(promise);
   Promise.all(promises).then(() => {
     console.timeEnd("execTime");
   });
 }
 
 // 翻訳結果を要素に置換
-function replaceContent(selector, translatedArray) {
-  document.querySelectorAll(selector).forEach((element, index) => {
-    if (index < translatedArray.length) {
-      element.textContent = translatedArray[index];
-    }
+function replaceContent(textNodes, translatedArray) {
+  textNodes.forEach((element, index) => {
+    element.textContent = translatedArray[index];
   });
 }
 
