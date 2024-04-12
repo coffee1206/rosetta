6995f9450abecbf978d7ae2695b35d000b4453fc
coffee1206 <cafemist1206@gmail.com>
2024-04-12 20:44:53 +0900

    セレクターのテキストノードのみ取得するよう変更・連想配列化

--- content.js
+++ content.js
@@ -18,38 +18,48 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // 各要素を取得、翻訳して置換する
 function exec() {
   console.time("exec_time");
-  const allNodes = Array.from(
-    document.querySelectorAll(
-      "body *:not(script):not(script *):not(code):not(code *):not(style):not(style *):not(link):not(meta):not(noscript):not(iframe):not(object):not(embed)"
-    )
-  )
-    .map((el) => Array.from(el.childNodes).filter((el) => el instanceof Text))
-    .flat();
-  const textNodes = [];
-  const rawTextArray = [];
+  //TODO ゴミみてえなDOMに対応する必要なし！！！！セレクターで取得するだけでヨシ！！！！リバート！！！！
+  const textNodes = []; // selectorsで指定したセレクターのエレメントとテキストをここに格納
+  const selectors = ["h1", "h2", "h3", "p", "li"];
+  const splittedNodes = [];
   const promises = [];
 
-  allNodes.forEach((element) => {
-    const textNodeContent = element.textContent.trim();
-    if (textNodeContent !== "") {
-      textNodes.push(element);
-      rawTextArray.push(textNodeContent);
-    }
+  // 連想配列としてelementとtextContentを格納
+  selectors.forEach((selector) => {
+    let index = 0;
+    document.querySelectorAll(selector).forEach((element) => {
+      let textNode = {
+        element: element,
+        elementIndex: index++,
+        textContent: element.textContent,
+      };
+      textNodes.push(textNode);
+    });
   });
 
-  // console.log(rawTextArray);
-  const promise = translate(rawTextArray)
+  console.log(textNodes);
+
+  textNodes.forEach((textNode, index) => {
+    let textCount = 0;
+    textCount += textNode.textContent.length;
+    splittedNodes.push(textNode);
+    if (textCount > 1000) {
+    }
+  });
+  const promise = translate(splittedNodes)
     .then((response) => {
-      //TODO テキストノードベースに変更したがここでコケてる　あとで直す
-      const translatedArray = JSON.parse(response.translatedText);
+      //TODO テキストノードベースに変更したがjson.parseでこけてる　あとで直す
+      const translatedArray = response.translatedText;
+      console.log(translatedArray);
+      const jsonparse = JSON.parse(translatedArray);
       console.log("input_tokens:" + response.input_tokens);
       console.log("output_tokens:" + response.output_tokens);
-      console.log(translatedArray);
+      console.log(jsonparse);
       replaceContent(textNodes, translatedArray);
       console.timeLog("exec_time");
     })
     .catch((error) => {
-      element.textContent = error;
+      console.error(error);
     });
   promises.push(promise);
   Promise.all(promises).then(() => {
