ecbdaf6703f219ad9caed6e1ca13c9e752bb2481
coffee1206 <cafemist1206@gmail.com>
2024-04-22 19:37:29 +0900

    viteの設定

---
+++ ts_compile_settings/vite.config.background_script.js
@@ -0,0 +1,22 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+ 
+export default defineConfig((opt) => {
+  return {
+    root: 'src',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: false,
+      rollupOptions: {
+        input: {
+          content_script: resolve(__dirname, '../src/background_script.ts'),
+        },
+        output: {
+          entryFileNames: '[name].js',
+          inlineDynamicImports: true,
+          format: 'iife',
+        },
+      },
+    },
+  };
+});
---
+++ vite.config.mjs
@@ -0,0 +1,38 @@
+import path from 'path';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+ 
+export default defineConfig((opt) => {
+  return {
+    root: 'src',
+    plugins: [
+      viteStaticCopy({
+          targets: [
+            {
+              src: path.resolve(__dirname, 'src/manifest.json'),
+              dest: './'
+            },
+            { 
+              src: path.resolve(__dirname, 'src/images/*'),
+              dest: './images' 
+            }
+          ]
+        })
+      ],
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          popup: resolve(__dirname, 'src/popup.html'),
+          option: resolve(__dirname, 'src/option/option.html'),
+        },
+        output: {
+          entryFileNames: '[name].js',
+          
+        },
+      },
+    },
+  };
+});
