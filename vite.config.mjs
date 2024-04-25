cf44d9298084eaf3dac874b4283c39224c326e35
coffee1206 <cafemist1206@gmail.com>
2024-04-25 19:42:46 +0900

    ｂビルドできるように修正

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,36 +1,35 @@
-import path from 'path';
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
- 
+import path from "path";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 export default defineConfig((opt) => {
   return {
-    root: 'src',
+    root: "src",
     plugins: [
       viteStaticCopy({
-          targets: [
-            {
-              src: path.resolve(__dirname, 'src/manifest.json'),
-              dest: './'
-            },
-            { 
-              src: path.resolve(__dirname, 'src/images/*'),
-              dest: './images' 
-            }
-          ]
-        })
-      ],
+        targets: [
+          {
+            src: path.resolve(__dirname, "src/manifest.json"),
+            dest: "./",
+          },
+          {
+            src: path.resolve(__dirname, "src/images/*"),
+            dest: "./images",
+          },
+        ],
+      }),
+    ],
     build: {
-      outDir: '../dist',
+      outDir: "../dist",
       emptyOutDir: true,
       rollupOptions: {
         input: {
-          popup: resolve(__dirname, 'src/popup.html'),
-          option: resolve(__dirname, 'src/option/option.html'),
+          popup: resolve(__dirname, "src/popup.html"),
+          option: resolve(__dirname, "src/option/option.html"),
         },
         output: {
-          entryFileNames: '[name].js',
-          
+          entryFileNames: "[name].js",
         },
       },
     },
